b2d644bfb8b59b037a1f1683a5003e9638f996ad
hasibmuhammad <hasibmu99@gmail.com>
2022-07-02 18:24:50 +0600

    Initail commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.get("/myget", (req, res) => {
+  console.log(req.body);
+
+  res.send(req.body);
+});
+
+app.listen(4001, () => console.log(`Server is running at port 4001...`));
